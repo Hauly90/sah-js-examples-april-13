21c6778399c0ca37576e702f03307ee9cfc257b8
Hauly90 <j.macicek@centrum.cz>
2020-04-23 17:07:53 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,4 +3,8 @@ let name = 'Adam';
 console.log(name + "'s apples");
 console.log('Hello World!');
 
-/* This is a JavaScript comment */
+$('h1').text('We are having a 10 minutes break until 17:05');
+
+/* 
+This is a JavaScript comment 
+*/
